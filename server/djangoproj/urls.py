a3e2a681f5c4ef52c873f2c2273bf5e0fd10300a
Harini062 <harinidevi200@gmail.com>
2025-11-30 08:02:46 -0500

    Build CarModel & CarMake

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
-    path('djangoapp/', include('djangoapp.urls')),
+    path('', include('djangoapp.urls')),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
